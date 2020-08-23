88bfc99394c63e49be9065ebb4c65886896382fa
Ranjan <Prakash_Ranjan@Dell.com>
2020-08-23 23:28:02 +0530

    Added a code for counting valleys.

---
+++ HakerRank/src/InterviewPreparationKit/warmUpChallenges/CountingValleys.java
@@ -0,0 +1,43 @@
+package InterviewPreparationKit.warmUpChallenges;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class CountingValleys {
+
+    // Complete the countingValleys function below.
+    static int countingValleys(int n, String s) {
+    	int count = 0;
+    	int test = 0;
+    	int previousState = 0;
+    	for(int i =0; i< s.length() ;i++)
+    	{
+    		if(s.charAt(i) == 'U') {
+    			test = test +1;
+    		}else {
+    			test = test -1;
+    		}
+    		if(test < 0 && previousState >= 0) {
+    			count++;
+    		}
+    		previousState = test;
+    	}
+    	return count;
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        int n = 8;
+        
+        String s = "UDDDUDUU";
+
+        int result = countingValleys(n, s);
+
+        System.out.println(result);
+    }
+}
